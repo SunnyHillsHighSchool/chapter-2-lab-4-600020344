dd029cc7a4ab3d83519e89da13e370f300778894
600020344 <77069279+600020344@users.noreply.github.com>
2021-03-29 10:07:34 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,10 +6,19 @@ import java.util.Arrays;
 import java.util.Iterator;
 import static java.lang.System.*;
 
-public class IteratorRemoverRunner
+public class Main
 {
 	public static void main ( String[] args )
 	{
 		//add test cases	
+    IteratorRemover ir = new IteratorRemover("a b c a b c a","a");
+    ir.remove();
+    System.out.println(ir);
+    ir.setTest("a b c d e f g h i j x x x x","x");
+    ir.remove();
+    System.out.println(ir);
+    ir.setTest("1 2 3 4 5 6 a b c a b c","b");
+    ir.remove();
+    System.out.println(ir);
 	}
 }
